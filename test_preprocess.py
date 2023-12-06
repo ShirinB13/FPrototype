3ebb3dc773d3b29d381c28cc9d0cc752a614ae4f
ShirinB13 <shirinbegum707@hotmail.com>
2023-12-06 16:02:25 +0000

    Fixing Cleaning Unit Tests

--- test_preprocess.py
+++ test_preprocess.py
@@ -2,27 +2,40 @@ import unittest
 from preprocess import clean_data
 
 class TestCleanData(unittest.TestCase):
-    def test_clean_data(self):
         # Case 1: testing with sample data file
+    def test_clean_data_length(self):
         cleaned_data = clean_data("test_data.txt")
-        self.assertIsNotNone(cleaned_data)
-        self.assertIsInstance(cleaned_data, list)
         self.assertGreater(len(cleaned_data), 0)
-        print("Test 1: Passed")
+    def test_clean_data_outputs_list(self):
+        cleaned_data = clean_data("test_data.txt")
+        self.assertIsInstance(cleaned_data, list)
+    def test_clean_data_output_length(self):
+        cleaned_data = clean_data("test_data.txt")
+        self.assertGreater(len(cleaned_data), 0)
 
         # Case 2: testing an empty data file
+    def test_empty_data_not_none(self):
         cleaned_empty_data = clean_data("empty_data.txt")
         self.assertIsNotNone(cleaned_empty_data)
+    def test_empty_data_outputs_list(self):
+        cleaned_empty_data = clean_data("empty_data.txt")
         self.assertIsInstance(cleaned_empty_data, list)
+    def test_empty_data_output_length(self):
+        cleaned_empty_data = clean_data("empty_data.txt")
         self.assertEqual(len(cleaned_empty_data), 0)
-        print("Test 2: Passed")
 
         # Case 3: testing data containing special characters
+    def test_special_chars_not_none(self):
         cleaned_special_chars = clean_data("test_data.txt")
         self.assertIsNotNone(cleaned_special_chars)
+    def test_special_chars_outputs_list(self):
+        cleaned_special_chars = clean_data("test_data.txt")
         self.assertIsInstance(cleaned_special_chars, list)
+    def test_special_chars_output_length(self):
+        cleaned_special_chars = clean_data("test_data.txt")
         self.assertGreater(len(cleaned_special_chars), 0)
-        self.assertNotRegex(cleaned_special_chars[0], r"[@#\\$%^&*()]+")  # Updated pattern to allow '!'
-        print("Test 3: Passed")
+    def test_special_chars_no_special_characters(self):
+        cleaned_special_chars = clean_data("test_data.txt")
+        self.assertNotRegex(cleaned_special_chars[0], r"[@#\\$%^&*()]+")
 
 # command to run tests: python -m unittest -v test_preprocess.py
